2d3c8e4899d3a2d2ad101c69e93bcd8596c18ed8
nchernys <natasha.e.chernysheva@gmail.com>
2024-02-10 15:04:35 -0500

    first commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$email= $_POST['email'];
+$request= $_POST['request'];
+ 
+$to = "natasha.e.chernysheva@gmail.com";
+$subject = "FROM WEB DEV PAGE";
+ 
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Request =" . $request;
+ 
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+header("Location:index.html");
+?>
